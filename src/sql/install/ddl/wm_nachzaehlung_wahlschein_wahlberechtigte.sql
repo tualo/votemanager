1e0969eaec8a376b2548dfe7044e7d32f24436ab
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-08-11 12:39:17 +0200

    renew rescan

--- src/sql/install/ddl/wm_nachzaehlung_wahlschein_wahlberechtigte.sql
+++ src/sql/install/ddl/wm_nachzaehlung_wahlschein_wahlberechtigte.sql
@@ -7,6 +7,8 @@ CREATE TABLE IF NOT EXISTS wm_nachzaehlung_wahlschein_wahlberechtigte(
     `wahlscheinstatus` int(11) DEFAULT 1,
     `abgabetyp` int(11) DEFAULT 0,
     primary key (`wahlberechtigte`, `wahlscheinstatus`, `abgabetyp`),
+
+    `blocknumber` varchar(30) NOT NULL DEFAULT '0',
     `login` varchar(50),
     `ctime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `mtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -14,10 +16,12 @@ CREATE TABLE IF NOT EXISTS wm_nachzaehlung_wahlschein_wahlberechtigte(
     KEY `idx_wm_nachzaehlung_wahlschein_wahlberechtigte_wahlberechtigte` (`wahlberechtigte`),
     KEY `idx_wm_nachzaehlung_wahlschein_wahlberechtigte_wahlscheinstatus` (`wahlscheinstatus`),
     KEY `idx_wm_nachzaehlung_wahlschein_wahlberechtigte_abgabetyp` (`abgabetyp`),
+    KEY `idx_wm_nachzaehlung_wahlschein_wahlberechtigte_blocknumber` (`blocknumber`),
     KEY `idx_wm_nachzaehlung_wahlschein_wahlberechtigte_login` (`login`),
 
     CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlberechtigte_wahlscheinstatus` FOREIGN KEY (`wahlscheinstatus`) REFERENCES `wahlscheinstatus` (`id`) ON DELETE restrict ON UPDATE CASCADE,
-    CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlberechtigte_abgabetyp` FOREIGN KEY (`abgabetyp`) REFERENCES `abgabetyp` (`id`) ON DELETE restrict ON UPDATE CASCADE
+    CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlberechtigte_abgabetyp` FOREIGN KEY (`abgabetyp`) REFERENCES `abgabetyp` (`id`) ON DELETE restrict ON UPDATE CASCADE,
+    CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlberechtigte_blocknumber` FOREIGN KEY (`blocknumber`) REFERENCES `wahlschein_blocknumbers` (`blocknumber`) ON DELETE restrict ON UPDATE CASCADE
 
 )
 WITH
--- src/sql/install/ddl/wm_nachzaehlung_wahlschein_wahlscheinnummer.sql
+++ src/sql/install/ddl/wm_nachzaehlung_wahlschein_wahlscheinnummer.sql
@@ -7,16 +7,20 @@ CREATE TABLE IF NOT EXISTS wm_nachzaehlung_wahlschein_wahlscheinnummer(
     `wahlscheinstatus` int(11) DEFAULT 1,
     `abgabetyp` int(11) DEFAULT 0,
     primary key (`wahlscheinnummer`, `wahlscheinstatus`, `abgabetyp`),
+    `blocknumber` varchar(30) NOT NULL DEFAULT '0',
     `login` varchar(50),
     `ctime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `mtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     KEY `idx_wm_nachzaehlung_wahlschein_wahlscheinnummer_wahlscheinnummer` (`wahlscheinnummer`),
     KEY `idx_wm_nachzaehlung_wahlschein_wahlscheinnummer_wahlscheinstatus` (`wahlscheinstatus`),
     KEY `idx_wm_nachzaehlung_wahlschein_wahlscheinnummer_abgabetyp` (`abgabetyp`),
+
+    KEY `idx_wm_nachzaehlung_wahlschein_wahlscheinnummer_blocknumber` (`blocknumber`),
     KEY `idx_wm_nachzaehlung_wahlschein_wahlscheinnummer_login` (`login`),
 
     CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlscheinnummer_wahlscheinstatus` FOREIGN KEY (`wahlscheinstatus`) REFERENCES `wahlscheinstatus` (`id`) ON DELETE restrict ON UPDATE CASCADE,
-    CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlscheinnummer_abgabetyp` FOREIGN KEY (`abgabetyp`) REFERENCES `abgabetyp` (`id`) ON DELETE restrict ON UPDATE CASCADE
+    CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlscheinnummer_abgabetyp` FOREIGN KEY (`abgabetyp`) REFERENCES `abgabetyp` (`id`) ON DELETE restrict ON UPDATE CASCADE,
+    CONSTRAINT `fk_wm_nachzaehlung_wahlschein_wahlscheinnummer_blocknumber` FOREIGN KEY (`blocknumber`) REFERENCES `wahlschein_blocknumbers` (`blocknumber`) ON DELETE restrict ON UPDATE CASCADE
 
 )
 WITH
