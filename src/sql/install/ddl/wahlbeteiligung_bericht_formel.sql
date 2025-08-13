037b086b999c5d005fb7903b4d3cb60a40adc2cd
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-08-13 09:19:57 +0200

    add readtable

--- src/sql/install/ddl/wahlbeteiligung_bericht_formel.sql
+++ src/sql/install/ddl/wahlbeteiligung_bericht_formel.sql
@@ -10,3 +10,39 @@ CREATE TABLE IF NOT EXISTS `wahlbeteiligung_bericht_formel` (
   PRIMARY KEY (`id` )
 ) ;
 
+
+
+create or replace view `view_readtable_wahlbeteiligung_bericht_formel` as
+select
+  wahlbeteiligung_bericht_formel.`id`,
+  wahlbeteiligung_bericht_formel.`login`,
+  wahlbeteiligung_bericht_formel.`aktiv`,
+  wahlbeteiligung_bericht_formel.`name`,
+  json_arrayagg(
+      if(`wahlbeteiligung_bericht_formel_nenner`.`id` is null, null,
+    json_object(
+      'id', `wahlbeteiligung_bericht_formel_nenner`.`id`,
+      'name', `wahlbeteiligung_bericht_formel_nenner`.`name`,
+      'aktiv', `wahlbeteiligung_bericht_formel_nenner`.`aktiv`,
+      'login', `wahlbeteiligung_bericht_formel_nenner`.`login`
+    ))
+  ) as nenner,
+  json_arrayagg(
+      if(`wahlbeteiligung_bericht_formel_teiler`.`id` is null, null,
+    json_object(
+      'id', `wahlbeteiligung_bericht_formel_teiler`.`id`,
+      'name', `wahlbeteiligung_bericht_formel_teiler`.`name`,
+      'aktiv', `wahlbeteiligung_bericht_formel_teiler`.`aktiv`,
+      'login', `wahlbeteiligung_bericht_formel_teiler`.`login`
+    ))
+  ) as teiler
+from
+  `wahlbeteiligung_bericht_formel`
+  left join wahlbeteiligung_bericht_formel_nenner
+  on wahlbeteiligung_bericht_formel_nenner.wahlbeteiligung_bericht_formel = wahlbeteiligung_bericht_formel.id
+    and wahlbeteiligung_bericht_formel_nenner.aktiv = 1
+  left join wahlbeteiligung_bericht_formel_teiler
+  on wahlbeteiligung_bericht_formel_teiler.wahlbeteiligung_bericht_formel = wahlbeteiligung_bericht_formel.id
+    and wahlbeteiligung_bericht_formel_teiler.aktiv = 1
+group by id
+;
