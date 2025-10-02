34f547e013019bc7e8a09ab853ee09ba4d278c2c
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-02 14:34:58 +0200

    fix can change

---
+++ src/sql/install/ddl/canChangeValue.sql
@@ -0,0 +1,32 @@
+DELIMITER //
+CREATE OR REPLACE FUNCTION `canChangeValue`(in_system_settings_id varchar(50) ) RETURNS tinyint(1)
+    DETERMINISTIC
+BEGIN
+  DECLARE lvl BOOLEAN DEFAULT FALSE;
+
+
+  IF EXISTS(
+      SELECT 
+        system_settings_user_access.system_settings_id  
+      FROM 
+        system_settings_user_access join view_session_groups 
+        on system_settings_user_access.groupname = view_session_groups.group 
+    WHERE 
+      system_settings_user_access.system_settings_id = in_system_settings_id  
+    ) THEN
+    SET lvl= TRUE;
+  ELSE
+    IF NOT EXISTS(select system_settings_id from system_settings where system_settings_id=in_system_settings_id ) THEN
+      SET lvl= TRUE;
+    END IF;
+  END IF;
+  RETURN (lvl);
+END //
+
+insert ignore into system_settings_user_access 
+(system_settings_id,groupname) values
+('erp/privatekey','administration'),
+('remote-erp/public','administration'),
+('remote-erp/token','administration'),
+('remote-erp/url','administration')
+//
