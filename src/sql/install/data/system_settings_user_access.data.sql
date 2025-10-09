6bd3705dc5b52b60d1717a36f258669f68ebea1d
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-09 11:17:11 +0200

    Version bump to 1.1.8

--- src/sql/install/data/system_settings_user_access.data.sql
+++ src/sql/install/data/system_settings_user_access.data.sql
@@ -1,3 +1,21 @@
 delimiter ;
+
 insert ignore into  system_settings_user_access 
 select system_settings_id,'administration' from system_settings ;
+
+
+
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('remote-erp/public','administration');
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('remote-erp/url','administration');
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('remote-erp/token','administration');
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('erp/privatekey','administration');
+
+insert ignore into  system_settings (system_settings_id) values 
+	('remote-erp/public'),
+  ('remote-erp/url'),
+  ('remote-erp/token') 
+;
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('remote-erp/public','administration');
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('remote-erp/url','administration');
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('remote-erp/token','administration');
+insert ignore into system_settings_user_access(system_settings_id,groupname) values ('erp/privatekey','administration');
