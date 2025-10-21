2e8c6c0d639bab11e480bf41f8235013c04b1b55
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-10-21 10:18:07 +0200

    add status

--- src/sql/install/ddl/trigger_wahlschein_bu_wm_register.sql
+++ src/sql/install/ddl/trigger_wahlschein_bu_wm_register.sql
@@ -1,37 +1,59 @@
 
 DELIMITER //
+
+
+insert ignore into `wahlscheinstatus` (`id`, `name`, `aktiv`, `barcode`, `farbe`, `ohne_wahlberechtigten`,`login`) values
+('-8','Zeichnunsgberechtigter unklar','0','FC999000','rgba(230, 23, 23, 1)','0','setup') //
+
 CREATE OR REPLACE TRIGGER `trigger_wahlschein_bu_wm_register`
 BEFORE
 UPDATE ON `wahlschein` FOR EACH ROW
 BEGIN
   IF (NEW.abgabetyp=2 and NEW.wahlscheinstatus=2) THEN
     IF NOT EXISTS (
-      select 1 
+      
+      
+      select 
+        JSON_VALUE(wm_wahlschein_register.formdata,'$.first_name') fn,
+        JSON_VALUE(wm_wahlschein_register.formdata,'$.last_name') ln,
+        wahlzeichnungsberechtigter.vorname,
+        wahlzeichnungsberechtigter.nachname,
+        if (
+            (
+                  (
+                    JSON_VALUE(wm_wahlschein_register.formdata,'$.first_name') = wahlzeichnungsberechtigter.vorname
+                    and JSON_VALUE(wm_wahlschein_register.formdata,'$.last_name') = wahlzeichnungsberechtigter.nachname
+                  ) or
+
+                  (
+                    JSON_VALUE(wm_wahlschein_register.formdata,'$.first_name') = wahlzeichnungsberechtigter.nachname
+                    and JSON_VALUE(wm_wahlschein_register.formdata,'$.last_name') = wahlzeichnungsberechtigter.vorname
+
+                  )
+                  ),
+                  'match',
+                  'no match'
+        ) status,
+        wm_wahlschein_register.*,
+        wahlschein.wahlscheinstatus
       from
         wm_wahlschein_register
         join
         wahlschein
           on wm_wahlschein_register.id = wahlschein.id
+          and wahlschein.id = NEW.id
         join 
           wahlberechtigte
-          on wahlberechtigte.identnummer = wahlschein.name
-        join 
+          on wahlberechtigte.id = wahlschein.wahlberechtigte
+        left join 
           wahlzeichnungsberechtigter
-          on wahlberechtigte.id = wahlzeichnungsberechtigter.wahlberechtigte
-        where
+          on 
+            wahlberechtigte.id = wahlzeichnungsberechtigter.wahlberechtigte
 
-          (
-            JSON_VALUE(wm_wahlschein_register.formdata,'$.first_name') = wahlzeichnungsberechtigter.vorname
-            and JSON_VALUE(wm_wahlschein_register.formdata,'$.last_name') = wahlzeichnungsberechtigter.nachname
-          ) or
+      having status='match'
 
-          (
-            JSON_VALUE(wm_wahlschein_register.formdata,'$.first_name') = wahlzeichnungsberechtigter.nachname
-            and JSON_VALUE(wm_wahlschein_register.formdata,'$.last_name') = wahlzeichnungsberechtigter.vorname
-
-          )
     ) THEN
-      set NEW.wahlscheinstatus=8;
+      set NEW.wahlscheinstatus=-8;
     END IF;
   END IF;
   
