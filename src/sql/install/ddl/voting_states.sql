5975332e469e18a34739efebc9713903e82adcc4
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-12-04 10:50:13 +0100

    Version bump to 1.1.27

--- src/sql/install/ddl/voting_states.sql
+++ src/sql/install/ddl/voting_states.sql
@@ -3,10 +3,11 @@ DELIMITER //
 
 CREATE TABLE IF NOT EXISTS `voting_states` (
     id varchar(20) NOT NULL,
-    constraint check(id in ('setup_phase', 'test_phase', 'production_phase', 'council_phase')),
+    constraint check(id in ('setup_phase', 'test_phase', 'production_phase', 'council_phase', 'reset_phase')),
     name varchar(255) NOT NULL,
     primary key (id)
 ) //
 
-insert ignore into voting_states (id,name) values ('setup_phase','Setup Phase'), ('test_phase','Test Phase'), ('production_phase','Wahldurchführung'), ('council_phase','Wahlauswertung') //
+insert ignore into voting_states (id,name) values 
+('setup_phase','Setup Phase'), ('test_phase','Test Phase'), ('production_phase','Wahldurchführung'), ('council_phase','Wahlauswertung'), ('reset_phase', 'Reset Phase') //
 
