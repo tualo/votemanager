5add13958b4fae52ef3b95fe17df9e0c1373a4c6
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-03-03 13:30:54 +0100

    Version bump to 1.1.67

--- src/sql/install/data/erweiterte_ds_rechte.sql
+++ src/sql/install/data/erweiterte_ds_rechte.sql
@@ -13,12 +13,8 @@ VALUES ('0','0','1','wahl_nachzaehlung','wm_nachzaehlung_wahlschein_wahlberechti
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) 
 VALUES ('0','0','1','wahl_nachzaehlung','wm_nachzaehlung_wahlschein_wahlscheinnummer','1') ;
 
-
-
-
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','blocked_voters') ;
-
 
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','wahlbezirk') ;
@@ -29,26 +25,20 @@ VALUES ('0','0','1','1','wahl_onlinewahl_sync','wahlgruppe') ;
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettel') ;
 
-
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettelgruppen') ;
-
 
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','kandidaten') ;
 
-
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','kandidaten_bilder') ;
-
 
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','kandidaten_bilder_typen') ;
 
-
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettel_stimmzettel_fusstexte') ;
-
 
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
 VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettel_fusstexte') ;
