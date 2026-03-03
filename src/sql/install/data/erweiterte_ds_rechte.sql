9809c41d92b6773f7e4d200f9d3dc0ce104f10b2
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2026-03-03 12:32:37 +0100

    Version bump to 1.1.66

--- src/sql/install/data/erweiterte_ds_rechte.sql
+++ src/sql/install/data/erweiterte_ds_rechte.sql
@@ -11,5 +11,50 @@ INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`wr
 VALUES ('0','0','1','wahl_nachzaehlung','wm_nachzaehlung_wahlschein_wahlberechtigte','1') ;
 
 INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`role`,`table_name`,`write`) 
-VALUES ('0','0','01','wahl_nachzaehlung','wm_nachzaehlung_wahlschein_wahlscheinnummer','1') ;
+VALUES ('0','0','1','wahl_nachzaehlung','wm_nachzaehlung_wahlschein_wahlscheinnummer','1') ;
 
+
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','blocked_voters') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','wahlbezirk') ;
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','wahlgruppe') ;
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettel') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettelgruppen') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','kandidaten') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','kandidaten_bilder') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','kandidaten_bilder_typen') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettel_stimmzettel_fusstexte') ;
+
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','stimmzettel_fusstexte') ;
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','ds_files') ;
+
+INSERT IGNORE INTO `ds_access` (`append`,`delete`,`read`,`write`,`role`,`table_name`) 
+VALUES ('0','0','1','1','wahl_onlinewahl_sync','ds_files_data') ;
